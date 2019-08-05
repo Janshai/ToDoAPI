11f944650d675b6c5251ceb68a9a886327eecc75
Janshai <jack.r.s.adams@gmail.com>
2019-08-05 20:35:19 +0100

    remove description field

--- app/models/todo.js
+++ app/models/todo.js
@@ -5,7 +5,6 @@ let Schema = mongoose.Schema;
 let TodoSchema = new Schema(
   {
     title: { type: String, required: true },
-    description: {type: String, required: false},
     createdAt: { type: Date, default: Date.now },
   },
   {
